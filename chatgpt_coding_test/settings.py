70ec5ae997e6932ed5722ac388ffabaa332fa06e
K-FactorAgency <115365117+K-FactorAgency@users.noreply.github.com>
2024-02-25 20:12:45 +0900

    Update settings.py

--- chatgpt_coding_test/settings.py
+++ chatgpt_coding_test/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '[PASTE YOUR OWN SECRET KEY TO HERE]'
+SECRET_KEY = '[dj3th0t@&k@m6r%p&&2t-p8m5jymjj84=(evz(=do+h$-j@%*w]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
